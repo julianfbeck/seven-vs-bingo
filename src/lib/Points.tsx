ff276dcc10ac7787461bcaeddb4783e7446c6584
Julian Beck <ju-fa-beck@t-online.de>
2022-09-25 18:35:24 +0200

    update points

--- src/lib/Points.tsx
+++ src/lib/Points.tsx
@@ -1,6 +1,4 @@
 import { BingoEntry, Projection } from "@prisma/client";
-import Head from "next/head";
-import Navbar from "../lib/navbar";
 import { Constants } from "../utils/points";
 
 interface PointsProps {
@@ -11,22 +9,32 @@ interface PointsProps {
 }
 
 const Points = ({ points, entries }: PointsProps) => {
+  const calcualtePoints = () => {
+    let total = entries
+      .filter((entry) => entry.projection.hasBecomeTrue)
+      .reduce((acc) => {
+        return acc + Constants.PointsPerCorrectEntry;
+      }, 0);
+
+    return (total += points.length * Constants.PointsPerCorrectRow);
+  };
+
   return (
     <>
       <div className="py-8 px-4 mx-auto max-w-screen-lg lg:py-10 lg:px-12">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Deine Punkte:
+          Deine Punkte
         </h1>
         <table className="w-full">
-          <tbody className="border-cyan-50">
+          <tbody className="border-slate-500 border-b bg-gray-800">
             {entries
               .filter((filter) => filter.projection.hasBecomeTrue)
               .map((entries) => (
                 <tr
                   key={entries.id}
-                  className=" bg-gray-800 hover:cursor-pointer hover:bg-gray-700 transform duration-150 last:border-0"
+                  className="hover:cursor-pointer hover:bg-gray-700 last:border-0"
                 >
-                  <td className="px-6 py-4 whitespace-no-wrap ">
+                  <td className="px-6 py-4 whitespace-no-wrap roun">
                     <div className="flex items-center">
                       <div className="ml-3">
                         <div className="text-sm leading-5 font-medium text-gray-100">
@@ -50,8 +58,55 @@ const Points = ({ points, entries }: PointsProps) => {
               ))}
           </tbody>
         </table>
+        {points.length > 0 && (
+          <table className="w-full">
+            <tbody>
+              {points.map((row, i) => (
+                <tr
+                  key={i}
+                  className=" bg-gray-800 hover:cursor-pointer hover:bg-gray-700 transform duration-150 last:border-0"
+                >
+                  <td className="px-6 py-4 whitespace-no-wrap ">
+                    <div className="flex items-center">
+                      <div className="ml-3">
+                        <div className="text-lg leading-5 font-medium text-gray-100">
+                          Bingo in den Feldern {row.map((field) => field + " ")}
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-center lg:table-cell">
+                    <div className="text-sm leading-5 text-gray-200">
+                      Bingo!
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-white text-lg font-bold text-right flex-none ">
+                    + {Constants.PointsPerCorrectRow}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <table className="w-full">
+          <tbody>
+            <tr className=" bg-green-600 hover:cursor-pointer hover:bg-gray-700 transform duration-150 last:border-0">
+              <td className="px-6 py-4 whitespace-no-wrap ">
+                <div className="flex items-center">
+                  <div className="ml-3">
+                    <div className="text-lg leading-5 font-medium text-gray-100">
+                      Punkte insgesamt
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-white text-lg font-bold text-right flex-none ">
+                + {calcualtePoints()}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      k
     </>
   );
 };
--- src/utils/points.ts
+++ src/utils/points.ts
@@ -1,3 +1,5 @@
 export class Constants {
   public static readonly PointsPerCorrectEntry = 5;
+
+  public static readonly PointsPerCorrectRow = 25;
 }

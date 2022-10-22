0973a466436899844ef9c6300a49f9169b11b816
Julian Beck <ju-fa-beck@t-online.de>
2022-10-22 20:31:39 +0200

    Updates layout

--- src/lib/Board.tsx
+++ src/lib/Board.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
 import { Field } from "./Field";
 import Points from "./Points";
@@ -34,7 +33,8 @@ const Board = ({ boardId }: BoardProps) => {
     <>
       <div className="py-3 px-4 mx-auto max-w-screen-lg lg:py-10 lg:px-12">
         <h1 className="text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
-          Dein <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-700 via-green-500 to-lime-500">
+          Dein{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-700 via-green-500 to-lime-500">
             Bingo Board
           </span>
         </h1>
@@ -45,23 +45,21 @@ const Board = ({ boardId }: BoardProps) => {
           Board Teilen
         </button>
       </div>
-      <div className="max-w-screen-sm mx-auto  container p-2^">
-        <div className="mx-auto  container ">
-          <TransformWrapper>
-            <TransformComponent>
-              <div className="grid grid-cols-5 gap-2 ">
-                {!isLoading &&
-                  fields.map((field) => (
-                    <Field
-                      key={field}
-                      fieldNumber={field}
-                      projection={getProjection(field)}
-                    />
-                  ))}
-              </div>
-            </TransformComponent>
-          </TransformWrapper>
-        </div>
+      <div className="max-w-screen-sm mx-auto container p-2">
+        <TransformWrapper>
+          <TransformComponent>
+            <div className="grid grid-cols-5 gap-2 overflow-visible">
+              {!isLoading &&
+                fields.map((field) => (
+                  <Field
+                    key={field}
+                    fieldNumber={field}
+                    projection={getProjection(field)}
+                  />
+                ))}
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
       </div>
       {points && entries && <Points entries={entries} points={points} />}
     </>
--- src/lib/Field.tsx
+++ src/lib/Field.tsx
@@ -15,9 +15,8 @@ export const Field: React.FC<TextProps> = ({ fieldNumber, projection }) => {
       return <CorrectField fieldNumber={fieldNumber} projection={projection} />;
     } else if (projection) {
       return <NormalField fieldNumber={fieldNumber} projection={projection} />;
-    } else {
-      return <EmptyField />;
     }
+    return <></>;
   }
   return (
     <>
@@ -38,20 +37,10 @@ export const Field: React.FC<TextProps> = ({ fieldNumber, projection }) => {
   );
 };
 
-const EmptyField = () => {
-  return (
-    <div className="container aspect-square rounded-xl p-3 bg-slate-800  opacity-70">
-      <div className="flex flex-col justify-center items-center h-full text-white text-xs hover:decoration-green-500 hover:underline">
-        Bingo Karte hinzufügen
-      </div>
-    </div>
-  );
-};
-
 const NormalField = ({ projection }: TextProps) => {
   return (
     <div className="container aspect-square  rounded-xl  bg-slate-800  text-white border border-white">
-      <div className="flex flex-col justify-center items-center h-full text-white text-xs lg:text-lg hover:decoration-green-500 hover:underline overflow-hidden">
+      <div className="flex flex-col text-[9px] justify-center items-center h-full text-white  sm:text-base hover:decoration-green-500 hover:underline overflow-hidden">
         {projection?.text}
       </div>
     </div>
@@ -61,7 +50,7 @@ const NormalField = ({ projection }: TextProps) => {
 const CorrectField = ({ projection }: TextProps) => {
   return (
     <div className="container aspect-square  rounded-xl bg-green-500  text-white border border-green-900">
-      <div className="flex flex-col justify-center items-center h-full text-white text-xs lg:text-lg hover:decoration-green-500 hover:underline overflow-hidden">
+      <div className="flex flex-col text-[9px] justify-center items-center h-full text-white sm:text-base hover:decoration-green-500 hover:underline overflow-hidden">
         {projection?.text}
       </div>
     </div>
